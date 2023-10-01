fa606efd225990e34e3ca5c439c5050956a3dadc
mzhdipu <mojammelhossaindipu@gmail.com>
2023-10-01 17:21:44 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [posts, setPosts] = useState([])
+  const [postsPerPage, setPostPerPage] = useState(10)
+  const [currentPage, setCurrentPage] = useState(1)
+
+  const indexOfLastPosts = currentPage * postsPerPage
+  const indexOfFirstPosts = indexOfLastPosts - postsPerPage
+  
+  const visiblePosts = posts.slice(indexOfFirstPosts, indexOfLastPosts)
+
+  useEffect(()=>{
+    fetch(`https://jsonplaceholder.typicode.com/posts`)
+    .then(res => res.json())
+    .then(data => setPosts(data))
+  },[])
+
+  const numberOfPages = Math.ceil(posts.length) / postsPerPage
+  const pages = [...Array(numberOfPages + 1).keys()].slice(1)
+  console.log(pages)
+
+  const prevPageHandler = () =>{
+    if(currentPage !== 1){
+      setCurrentPage(currentPage - 1)
+    }
+  }
+
+  const nextPageHandler = () =>{
+    if(currentPage !== numberOfPages){
+      setCurrentPage(currentPage + 1)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div>
+        {
+          visiblePosts.map(post => <p key={post.id}>{post.id} {post.title}</p>)
+        }
+      </div>
+      
+      <button onClick={prevPageHandler} >Prev</button>
+      {
+        pages.map(page => <button
+          key={page}
+          onClick={()=>setCurrentPage(page)}
+        >{page}</button>)
+      }
+      <button onClick={nextPageHandler}>Next</button>
+
+      <br /><br /><br />
     </div>
   );
 }
